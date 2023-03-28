bc352e8ff0a82681450c79960869c233174ea840
Marcello <marcelloaltermundi@gmail.com>
2023-03-28 18:30:15 -0300

    feat: configura dockerfiles e model Team

---
+++ app/backend/src/database/models/Team.ts
@@ -0,0 +1,41 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+
+class Team extends Model {
+  declare id: number;
+
+  declare teamName: string;
+}
+
+Team.init(
+  {
+    id: {
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
+    },
+    teamName: DataTypes.STRING,
+  },
+  {
+    underscored: true,
+    timestamps: false,
+    tableName: 'teams',
+    sequelize: db,
+  },
+);
+
+// Team.hasMany(Match, )
+
+/**
+  * `Workaround` para aplicar as associations em TS:
+  * Associations 1:N devem ficar em uma das instâncias de modelo
+  * */
+
+// OtherModel.belongsTo(Example, { foreignKey: 'campoA', as: 'campoEstrangeiroA' });
+// OtherModel.belongsTo(Example, { foreignKey: 'campoB', as: 'campoEstrangeiroB' });
+
+// Example.hasMany(OtherModel, { foreignKey: 'campoC', as: 'campoEstrangeiroC' });
+// Example.hasMany(OtherModel, { foreignKey: 'campoD', as: 'campoEstrangeiroD' });
+
+export default Team;
